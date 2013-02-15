c88cefb1a12f35a8554fc783074883cbc558100a
Christoph Buchner <bilderbuchi@phononoia.at>
2013-02-15 20:18:43 +0100

    Use request.access_route for determining origin of request to get through the Openshift servers.

--- ofCodeStyleGuard.py
+++ ofCodeStyleGuard.py
@@ -17,19 +17,21 @@ logging.getLogger('urllib3').setLevel(logging.WARNING)
 def api_pr():
 	""" React to a received POST request"""
 	LOGGER.info("Received POST request.")
-
-	if request.remote_addr not in styleguard.cfg['github_ips']:
-		LOGGER.warning("Origin of request UNKNOWN: " + request.remote_addr)
+	LOGGER.debug('Access route: ' + str(request.access_route[:]))
+	origin = request.access_route[0]
+	# was using request.remote_addr. access_route could possibly be spoofed
+	if origin not in styleguard.cfg['github_ips']:
+		LOGGER.warning("Origin of request UNKNOWN: " + origin)
 		return 'Error'
 	# TODO: 'proper' return objects here and below
 	else:
-		LOGGER.debug("Origin of request: " + request.remote_addr)
+		LOGGER.debug("Origin of request: " + origin)
 
 	try:
 		payload = json.loads(request.form['payload'])
 	except KeyError:
 		# crutch: if an invalid request arrives locally, load a json file directly
-		if request.remote_addr == '127.0.0.1':
+		if origin == '127.0.0.1':
 			location = os.getenv('OPENSHIFT_REPO_DIR', '')
 			with open(os.path.join(location, 'sample_payload.json'), 'r') as sample:
 				payload = json.load(sample)
